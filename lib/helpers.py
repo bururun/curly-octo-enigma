152366cc77fbc824663ee7bb5cade18fa230510a
bururun <bururun@users.noreply.github.com>
2025-10-30 18:58:24 +0200

    test: update error messages

--- lib/helpers.py
+++ lib/helpers.py
@@ -51,3 +51,14 @@ def helper_function_31(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_34(x):
+    """Helper function for iteration 34."""
+    return x * 34
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
