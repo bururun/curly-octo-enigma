b463aa9a0892001dcc4169ade73cb157390ddedf
bururun <bururun@users.noreply.github.com>
2026-01-28 08:36:03 +0200

    style: fix security vulnerability

--- lib/helpers.py
+++ lib/helpers.py
@@ -7,3 +7,14 @@ def helper_function_8(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_23(x):
+    """Helper function for iteration 23."""
+    return x * 23
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
